f6d051741151e8bb41b58deebbaad2010387073e
Philo Kaulkin <Phil0nator@users.noreply.github.com>
2020-02-23 16:38:26 -0800

    setting up new system to handle textures

---
+++ TextureHandling.cpp
@@ -0,0 +1,23 @@
+namespace TextureHandling{
+  class TextureHandler{
+
+    vector<SDL_Texture* > queuedDestination;
+    vector<SDL_Surface* > queuedSources;
+    vector<SDL_Texture* > allTextures;
+
+    
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+}
